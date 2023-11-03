2b80b1ce6fc5c61587b4e8fa153115d75314c7bf
woo3145 <lcwoo3145@gmail.com>
2023-11-03 13:44:32 +0900

    ✨(feature/mobile-ui) -  Header 모바일에서 스크롤 내릴때 숨기기

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,22 +1,48 @@
+'use client';
 import Link from 'next/link';
 import { DarkModeToggle } from '@/components/DarkModeToggle';
 import { UserMenu } from '@/components/common/UserMenu';
 import { BrandLogo } from '@/components/common/BrandLogo';
-import { getSessionUserServerAction } from '@/modules/user/domain/usecases/getSessionUserServerAction';
+import { SessionUser } from '@/modules/user/domain/user';
+import { useEffect, useRef, useState } from 'react';
+import { cn } from '@/lib/utils';
 
-export const Header = async () => {
-  const user = await getSessionUserServerAction();
+interface Props {
+  user: SessionUser | null;
+}
+
+export const Header = ({ user }: Props) => {
+  const [isVisible, setIsVisible] = useState(true);
+  const lastScrollTop = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const st = window.scrollY || document.documentElement.scrollTop;
+      if (20 <= st && st > lastScrollTop.current) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      lastScrollTop.current = st <= 0 ? 0 : st;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollTop]);
+
   return (
-    <header className="fixed top-0 left-0 w-full z-40 border-b border-border bg-background">
+    <header
+      className={cn(
+        'fixed top-0 left-0 w-full z-50 border-b border-border bg-background transition-transform duration-300',
+        isVisible ? 'translate-y-0' : '-translate-y-20 lg:translate-y-0'
+      )}
+    >
       <div className="flex items-center justify-between w-full h-16 px-4 mx-auto font-medium transition-colors duration-200">
         <section className="flex lg:flex-shrink-0 lg:w-52">
           <Link href="/">
             <BrandLogo size="md" />
           </Link>
         </section>
-        {/* <section className="flex flex-auto w-full">
-          <HeaderNavigation />
-        </section> */}
         <section className="flex items-center gap-4 flex-shrink-0">
           <UserMenu user={user} />
           <DarkModeToggle />
